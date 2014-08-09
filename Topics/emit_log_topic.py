fbf26ef791ca2c86c40cbcf74944a5af5bfdcea0
MiguelLunaHernandez <MiguelLunaHernandez@gmail.com>
2014-08-09 11:05:29 -0500

    implementation RPC with RabbitMQ

--- Topics/emit_log_topic.py
+++ Topics/emit_log_topic.py
@@ -6,12 +6,12 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
 	host='localhost'))
 channel = connection.channel()
 
-channgel.exchange_declare(exchange='topic_log',
+channel.exchange_declare(exchange='topic_logs',
 						  type='topic')
 
 routing_key = sys.argv[1] if len(sys.argv) > 1 else 'anonymous.info'
 message = ' '.join(sys.argv[2:]) or 'Hello world!'
-channel.basic_publish(exchange='topic_log',
+channel.basic_publish(exchange='topic_logs',
 					  routing_key=routing_key,
 					  body=message)
 
--- Topics/receive_logs_topic.py
+++ Topics/receive_logs_topic.py
@@ -6,7 +6,7 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
 	host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='topic_log',
+channel.exchange_declare(exchange='topic_logs',
 						 type='topic')
 
 result = channel.queue_declare(exclusive=True)
@@ -14,7 +14,7 @@ queue_name = result.method.queue
 
 binding_keys = sys.argv[1:]
 if not binding_keys:
-	print >> sys.stderr, "Usage: s% [binding_keys]..." % (sys.argv[0],)
+	print >> sys.stderr, "Usage: %s [binding_keys]..." % (sys.argv[0],)
 	sys.exit(1)
 
 for binding_key in binding_keys:
@@ -25,10 +25,10 @@ for binding_key in binding_keys:
 print " [x] Wainting for logs. To exit press CTRL+C"
 
 def callback(ch, method, properties, body):
-	print " [x] %r:%r" % (method.routing_key,nody,)
+	print " [x] %r:%r" % (method.routing_key,body,)
 
 channel.basic_consume(callback,
 					  queue=queue_name,
 					  no_ack=True)
 
-channel.star_consuming()
+channel.start_consuming()
